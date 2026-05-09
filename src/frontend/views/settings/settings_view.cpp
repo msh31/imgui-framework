d8c595641d84729910acb89d077954c0dcc66b79
Marco <marco@marco007.dev>
2026-05-09 22:11:56 +0200

    feat: add open config button in settings
    also ported from savemanager

--- src/frontend/views/settings/settings_view.cpp
+++ src/frontend/views/settings/settings_view.cpp
@@ -12,6 +12,32 @@ void SettingsView::render(Config& cfg) {
 
     if(ImGui::BeginPopupModal("Settings", &open_popup, ImGuiWindowFlags_NoResize)) {
         ImGui::Checkbox("Dark Mode", &cfg.settings.dark_mode);
+        if(ImGui::Button("Open config")) {
+#ifdef __linux__
+            pid_t pid = fork();
+            if (pid == 0) {
+                execl("/usr/bin/xdg-open", "xdg-open", paths::config_dir().string().c_str(), nullptr);
+                _exit(1);
+            }
+#endif
+#ifdef _WIN32
+            ShellExecuteA(NULL, "open", paths::config_dir().string().c_str(), NULL, NULL, SW_SHOWDEFAULT);
+#endif
+#ifdef __APPLE__
+
+            extern char **environ;
+            pid_t pid;
+            std::string path = paths::config_dir().string();
+
+            const char* argv[] = { "open", path.c_str(), nullptr };
+            int status = posix_spawn(&pid, "/usr/bin/open", nullptr, nullptr, (char* const*)argv, environ);
+            if (status == 0) {
+                waitpid(pid, &status, 0);
+            } 
+#endif
+    }
+    ImGui::SetItemTooltip("Opens your file manager to the config directory.");
+
         ImGui::EndPopup();
     }
 }
