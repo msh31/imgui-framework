12364e5cadd8f0adb239ff385c6c93658eae27e7
Marco <marco@marco007.dev>
2026-05-09 22:37:28 +0200

    fix: item width by using push & pop

--- src/frontend/views/home/home_view.cpp
+++ src/frontend/views/home/home_view.cpp
@@ -8,17 +8,24 @@ void HomeView::on_enter() {
 void HomeView::render(Config& cfg) {
     ImGui::Text("This is a the home view, you can add new views yourself by exploring the code. It's quite simple.");
 
-    // ImGui::SetNextItemWidth(100.0f);
+    ImGui::PushItemWidth(150.0f);
     ImGui::InputText("InputText", &input_str);
     ImGui::InputDouble("InputDouble", &input_double);
     ImGui::InputFloat("InputFloat", &input_float);
     ImGui::SameLine();
+    ImGui::Dummy({20.0f, 5.0f});
+    ImGui::SameLine();
     ImGui::SliderFloat("SliderFloat", &slider_step_f, slider_min_f, slider_max_f);
     ImGui::InputInt("InputInt", &input_int);
     ImGui::SameLine();
+    ImGui::Dummy({20.0f, 5.0f});
+    ImGui::SameLine();
     ImGui::SliderInt("SliderInt", &slider_step_i, slider_min_i, slider_max_i);
+    ImGui::PopItemWidth();
 
     ImGui::Checkbox("Checkbox", &checkbox_toggle);
+
+    ImGui::SetNextItemWidth(150.0f);
     if (ImGui::BeginCombo("Combo", selected_fruit.c_str())) {
         for (const auto& fruit : fruits) {
             if (ImGui::Selectable(fruit.c_str(), fruit == selected_fruit)) {
