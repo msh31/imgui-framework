2bef57afbaeb92d46248f1736d6ed91beb40e376
Marco <marco@marco007.dev>
2026-05-09 21:47:03 +0200

    change: settings window size

--- src/frontend/views/settings/settings_view.cpp
+++ src/frontend/views/settings/settings_view.cpp
@@ -7,7 +7,7 @@ void SettingsView::on_enter() {
 void SettingsView::render(Config& cfg) {
     ImVec2 center = ImGui::GetMainViewport()->GetCenter();
     ImGui::SetNextWindowPos(center, ImGuiCond_Always, {0.5f, 0.5f});
-    ImGui::SetNextWindowSize({700, 400});
+    ImGui::SetNextWindowSize({400, 200});
     bool open_popup = true;
 
     if(ImGui::BeginPopupModal("Settings", &open_popup, ImGuiWindowFlags_NoResize)) {
