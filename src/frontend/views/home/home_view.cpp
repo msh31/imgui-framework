e1a4115d880d4e3e7962b2c1675676dcdebac23b
Marco <marco@marco007.dev>
2026-05-09 23:40:47 +0200

    feat: add tree node for demo purposes

--- src/frontend/views/home/home_view.cpp
+++ src/frontend/views/home/home_view.cpp
@@ -8,54 +8,63 @@ void HomeView::on_enter() {
 void HomeView::render(Config& cfg) {
     ImGui::Text("This is a the home view, you can add new views yourself by exploring the code. It's quite simple.");
 
-    ImGui::PushItemWidth(150.0f);
-    ImGui::InputText("InputText", &input_str);
-    ImGui::InputDouble("InputDouble", &input_double);
-    ImGui::InputFloat("InputFloat", &input_float);
-    ImGui::SameLine();
-    ImGui::Dummy({20.0f, 5.0f});
-    ImGui::SameLine();
-    ImGui::SliderFloat("SliderFloat", &slider_step_f, slider_min_f, slider_max_f);
-    ImGui::InputInt("InputInt", &input_int);
-    ImGui::SameLine();
-    ImGui::Dummy({20.0f, 5.0f});
-    ImGui::SameLine();
-    ImGui::SliderInt("SliderInt", &slider_step_i, slider_min_i, slider_max_i);
-    ImGui::PopItemWidth();
+    if (ImGui::TreeNode("Inputs")) {
+        ImGui::PushItemWidth(150.0f);
+        ImGui::InputText("InputText", &input_str);
+        ImGui::InputDouble("InputDouble", &input_double);
+        ImGui::InputFloat("InputFloat", &input_float);
+        ImGui::SameLine();
+        ImGui::Dummy({20.0f, 5.0f});
+        ImGui::SameLine();
+        ImGui::SliderFloat("SliderFloat", &slider_step_f, slider_min_f, slider_max_f);
+        ImGui::InputInt("InputInt", &input_int);
+        ImGui::SameLine();
+        ImGui::Dummy({20.0f, 5.0f});
+        ImGui::SameLine();
+        ImGui::SliderInt("SliderInt", &slider_step_i, slider_min_i, slider_max_i);
+        ImGui::PopItemWidth();
+        ImGui::TreePop();
+    }
 
-    ImGui::Checkbox("Checkbox", &checkbox_toggle);
+    if (ImGui::TreeNode("Selection")) {
+        ImGui::Checkbox("Checkbox", &checkbox_toggle);
 
-    ImGui::SetNextItemWidth(200.0f);
-    if (ImGui::BeginCombo("Combo", selected_fruit.c_str())) {
-        for (const auto& fruit : fruits) {
-            if (ImGui::Selectable(fruit.c_str(), fruit == selected_fruit)) {
-                selected_fruit = fruit;
+        ImGui::SetNextItemWidth(200.0f);
+        if (ImGui::BeginCombo("Combo", selected_fruit.c_str())) {
+            for (const auto& fruit : fruits) {
+                if (ImGui::Selectable(fruit.c_str(), fruit == selected_fruit)) {
+                    selected_fruit = fruit;
+                }
             }
+            ImGui::EndCombo();
         }
-        ImGui::EndCombo();
-    }
 
-    ImGui::ColorEdit4("Color Picker", color); //can be used in if statement
-
-    ImGui::SetNextItemWidth(200.0f);
-    if(ImGui::BeginListBox("Listbox")) {
-        for (const auto& meat : meats) {
-            if (ImGui::Selectable(meat.c_str(), meat == selected_meat)) {
-                selected_meat = meat;
+        ImGui::SetNextItemWidth(200.0f);
+        if(ImGui::BeginListBox("Listbox")) {
+            for (const auto& meat : meats) {
+                if (ImGui::Selectable(meat.c_str(), meat == selected_meat)) {
+                    selected_meat = meat;
+                }
             }
+            ImGui::EndListBox();
         }
-        ImGui::EndListBox();
-    }
-    ImGui::SameLine();
-    ImGui::Text("| Selected: %s", selected_meat.c_str());
+        ImGui::SameLine();
+        ImGui::Text("| Selected: %s", selected_meat.c_str());
 
-    if(ImGui::RadioButton("RadioButton", radio_toggle)) {
-        radio_toggle = !radio_toggle;
+        if(ImGui::RadioButton("RadioButton", radio_toggle)) {
+            radio_toggle = !radio_toggle;
+        }
+        ImGui::SameLine();
+        ImGui::Text("| Status: %b", radio_toggle);
+        ImGui::TreePop();
     }
-    ImGui::SameLine();
-    ImGui::Text("| Status: %b", radio_toggle);
 
-    ImGui::ProgressBar(fmod(ImGui::GetTime() * 0.3f, 1.0f), {300.f, 0.f});
+    if (ImGui::TreeNode("Color && Progress")) {
+        ImGui::ColorEdit4("Color Picker", color);
+
+        ImGui::ProgressBar(fmod(ImGui::GetTime() * 0.3f, 1.0f), {300.f, 0.f});
+        ImGui::TreePop();
+    }
 }
 
 void HomeView::on_exit() {
