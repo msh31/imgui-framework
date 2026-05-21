211a7d7b892402c7ddc87210c62e75a2bcf67e03
Marco <marco@marco007.dev>
2026-05-21 14:53:37 +0200

    switch to sidebar layout for the demo

--- src/app/app.hpp
+++ src/app/app.hpp
@@ -4,8 +4,8 @@
 #include <backend/ui_manager/ui_manager.hpp>
 
 // #include <frontend/layout/tabbar/tabbar.hpp>
-// #include <frontend/layout/sidebar/sidebar.hpp>
-#include <frontend/layout/ribbon/ribbon.hpp>
+#include <frontend/layout/sidebar/sidebar.hpp>
+// #include <frontend/layout/ribbon/ribbon.hpp>
 
 class CApp {
 public:
@@ -17,8 +17,8 @@ private:
 
     CConfig m_config;
     // CUIManager m_ui_manager { std::make_unique<CTabbarShell>() };
-    // CUIManager m_ui_manager { std::make_unique<CSidebarShell>() };
-    CUIManager m_ui_manager { std::make_unique<CRibbonShell>() };
+    CUIManager m_ui_manager { std::make_unique<CSidebarShell>() };
+    // CUIManager m_ui_manager { std::make_unique<CRibbonShell>() };
 
     bool m_toggle_a = false, m_toggle_b = false, m_toggle_c = true,
          m_toggle_d = false, m_toggle_e = false;
