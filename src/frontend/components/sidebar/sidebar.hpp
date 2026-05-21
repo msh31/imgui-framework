4e5491c2690c5b0cebce97c98d6ffe4ee20cd1b8
Marco <marco@marco007.dev>
2026-05-22 00:04:14 +0200

    chore: remove ccomponent - dead code

--- src/frontend/components/sidebar/sidebar.hpp
+++ src/frontend/components/sidebar/sidebar.hpp
@@ -1,16 +1,15 @@
 #pragma once
-#include <frontend/components/component.hpp>
 #include <frontend/views/base_view.hpp>
 
-class CSideBar : public CComponent {
+class CSideBar {
     public:
-        CBaseView* render(CBaseView* active);
-        void add_item(CBaseView::ViewItem item);
-        void set_settings_view(CBaseView* view); //a little odd but i cant think of anything better right now
+        CBaseView *render( CBaseView *active );
+        void add_item( CBaseView::ViewItem item );
+        void set_settings_view( CBaseView *view ); // a little odd but i cant think of anything better right now
 
     private:
-        bool nav_button(const char* icon, const char* label, bool active, float width);
+        bool nav_button( const char *icon, const char *label, bool active, float width );
 
         std::vector<CBaseView::ViewItem> m_items;
-        CBaseView* m_settings;
+        CBaseView *m_settings;
 };
