b400a9586769cc77a89076cf5c9dd0b7359999b4
Marco <marco@marco007.dev>
2026-05-21 23:30:31 +0200

    feat: implement CCache template

---
+++ src/backend/cache.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <backend/task_runner/task_runner.hpp>
+
+template <typename T> class CCache {
+    public:
+        ~CCache( );
+
+        void refresh( std::function<T( )> fun ) {
+            if ( m_is_refreshing ) return;
+
+            m_is_refreshing = true;
+            std::shared_ptr<T> slot = std::make_shared<T>( );
+
+            m_taskrunner.run( [this, slot, fun] { *slot = fun( ); },
+                              [this, slot] {
+                                  m_current_snapshot = *slot;
+                                  m_is_refreshing = false;
+                              } );
+        }
+        const T &get( ) { return m_current_snapshot; }
+        bool is_refreshing( ) { return m_is_refreshing; }
+
+    private:
+        T m_current_snapshot = { };
+        std::atomic<bool> m_is_refreshing = false;
+        CTaskRunner m_taskrunner;
+};
