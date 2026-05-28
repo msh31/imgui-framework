1b57fb6108ce945a570c773a16ad6085f4bbd0d8
Marco <marco@marco007.dev>
2026-05-28 17:45:50 +0200

    refactor: extract open_in_file_manager
    stolen from savemanager

--- src/backend/utils.hpp
+++ src/backend/utils.hpp
@@ -1,9 +1,60 @@
 #pragma once
 
+#ifdef __APPLE__
+    #include <spawn.h>
+    #include <sys/wait.h>
+#endif
+#ifdef __linux__
+    #include <sys/wait.h>
+#endif
+#ifdef _WIN32
+    #include <shellapi.h>
+#endif
+
 inline std::string get_username( ) {
 #if defined( __linux__ ) || defined( __APPLE__ )
     return std::getenv( "USER" );
 #else
     return std::getenv( "USERNAME" );
+#endif
+}
+
+inline void open_in_file_manager( const char* path ) {
+#ifdef __linux__
+    pid_t pid = fork( );
+    pid_t w   = 0;
+    int   status;
+
+    if ( pid > 0 ) {
+        w = waitpid( pid, &status, 0 );
+        if ( w == -1 ) {
+            SPDLOG_ERROR( "waitpid failed: {}", EXIT_FAILURE );
+            exit( EXIT_FAILURE );
+        }
+    }
+
+    if ( pid == 0 ) {
+        pid_t g_pid = fork( );
+
+        if ( g_pid == 0 ) {
+            execl( "/usr/bin/xdg-open", "xdg-open", path, nullptr );
+            _exit( 1 );
+        }
+
+        if ( g_pid > 0 ) _exit( 0 );
+    }
+#endif
+#ifdef _WIN32
+    ShellExecuteA( NULL, "open", path, NULL, NULL, SW_SHOWDEFAULT );
+#endif
+#ifdef __APPLE__
+    extern char** environ;
+    pid_t         pid;
+
+    const char* argv[] = { "open", path, nullptr };
+    int         status = posix_spawn( &pid, "/usr/bin/open", nullptr, nullptr, (char* const*)argv, environ );
+    if ( status == 0 ) {
+        waitpid( pid, &status, 0 );
+    }
 #endif
 }
--- src/frontend/views/settings/settings_view.cpp
+++ src/frontend/views/settings/settings_view.cpp
@@ -1,12 +1,5 @@
 #include "settings_view.hpp"
-
-#ifdef __APPLE__
-    #include <spawn.h>
-    #include <sys/wait.h>
-#endif
-#ifdef _WIN32
-    #include <shellapi.h>
-#endif
+#include <backend/utils.hpp>
 
 CSettingsView::CSettingsView( CConfig& cfg ) : m_config( cfg ) {};
 
@@ -16,27 +9,7 @@ void CSettingsView::render( ) {
     ImGui::Checkbox( "Dark Mode", &m_config.settings.dark_mode );
 
     if ( ImGui::Button( "Open config" ) ) {
-#ifdef __linux__
-        pid_t pid = fork( );
-        if ( pid == 0 ) {
-            execl( "/usr/bin/xdg-open", "xdg-open", paths::config_dir( ).string( ).c_str( ), nullptr );
-            _exit( 1 );
-        }
-#endif
-#ifdef _WIN32
-        ShellExecuteA( NULL, "open", paths::config_dir( ).string( ).c_str( ), NULL, NULL, SW_SHOWDEFAULT );
-#endif
-#ifdef __APPLE__
-        extern char** environ;
-        pid_t         pid;
-        std::string   path = paths::config_dir( ).string( );
-
-        const char* argv[] = { "open", path.c_str( ), nullptr };
-        int         status = posix_spawn( &pid, "/usr/bin/open", nullptr, nullptr, (char* const*)argv, environ );
-        if ( status == 0 ) {
-            waitpid( pid, &status, 0 );
-        }
-#endif
+        open_in_file_manager( paths::config_dir( ).string( ).c_str( ) );
     }
     ImGui::SetItemTooltip( "Opens your file manager to the config directory." );
 }
