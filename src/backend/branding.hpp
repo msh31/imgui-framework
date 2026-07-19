cb92d0e141258321d4efcbc481feb5a650ee1f8d
Marco <marco@marco007.dev>
2026-07-19 02:21:11 +0200

    chore: add backend/branding.hpp
    replaces the soon to be deleted constants.hppk

---
+++ src/backend/branding.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+// The only file you should need to touch when reusing this framework as a template.
+constexpr std::string APP_NAME    = "ImGui Framework";
+constexpr std::string APP_AUTHOR  = "marco007";
+constexpr std::string APP_VERSION = "v1.0.0";
